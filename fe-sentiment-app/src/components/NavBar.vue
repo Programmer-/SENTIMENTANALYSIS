15306b05918832afe1cb199fdfa55002ac971dde
Justus Njuru <justnjuru@gmail.com>
2021-05-20 01:48:46 +0300

    Some Frontend updates

--- fe-sentiment-app/src/components/NavBar.vue
+++ fe-sentiment-app/src/components/NavBar.vue
@@ -1,6 +1,6 @@
 <template>
     <div>
-        <b-navbar toggleable="lg" type="dark" variant="primary">
+        <b-navbar toggleable="lg" type="dark" variant="secondary">
             <b-navbar-brand href="#">Sentiment App</b-navbar-brand>
 
             <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
--- fe-sentiment-app/src/views/Home.vue
+++ fe-sentiment-app/src/views/Home.vue
@@ -3,8 +3,8 @@
     <b-jumbotron header="Tweet Sentiment Dashboard" lead="Sentiment Analysis Web Application With Bootstrap4, VueJS and Python Flask">
       <p>This app help you to analyze sentiment of a hashtag</p>
       <div class="mb-3"><b>Follow Me</b></div>
-      <a class="btn btn-info mr-2" href="https://medium.com/@deavenditama">Medium</a>
-      <a class="btn btn-primary" href="https://www.facebook.com/DeaVenditama/">Facebook</a>
+      <a class="btn btn-dark mr-2" href="https://github.com/Programmer-">GitHub</a>
+      <a class="btn btn-primary" href="https://www.facebook.com/jahntel.runjez/">Facebook</a>
     </b-jumbotron>
   </div>
 </template>
